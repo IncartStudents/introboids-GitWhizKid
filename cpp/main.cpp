b41406d1f9a5e2223100403b57a3cfd1885c3b79
GitWhizKid <forvoxchannel@gmail.com>
2024-11-04 15:02:46 +0300

    Boids Algorithm

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -14,94 +14,6 @@ using namespace std;
 int W = 0;
 int H = 0;
 
-class CustomImGui : public UseImGui {
-public:
-	virtual void Update() override {
-
-		// render your GUI
-		static float f = 0.0f;
-		static int counter = 0;
-
-		ImGui::Begin("CustomImGui Hello, world!");              // Create a window called "Hello, world!" and append into it.
-
-		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-
-		bool clear_color_changed = ImGui::ColorEdit3("clear color", (float*)clear_color); // Edit 3 floats representing a color
-
-		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-			counter++;
-		ImGui::SameLine();
-		ImGui::Text("counter = %d", counter);
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-		ImGui::Button("Rhythm1");
-		ImGui::Text("display size: %d, %d", H, W);
-
-        static int e = 0;
-		ImGui::RadioButton("Привет!", &e, 0); // ImGui::SameLine();
-		//ImGui::RadioButton(u8"Привет!", &e, 1); // ImGui::SameLine();
-		ImGui::RadioButton("radio c", &e, 2);
-
-		if (ImGui::TreeNode("Дерево"))
-		{
-			// Using the generic BeginListBox() API, you have full control over how to display the combo contents.
-			// (your selection data could be an index, a pointer to the object, an id for the object, a flag intrusively
-			// stored in the object itself, etc.)
-			const char* items[] = { "AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "GGGG", "HHHH", "IIII", "JJJJ", "KKKK", "LLLLLLL", "MMMM", "OOOOOOO" };
-			static int item_current_idx = 0; // Here we store our selection data as an index.
-			if (ImGui::BeginListBox("listbox 1"))
-			{
-				for (int n = 0; n < IM_ARRAYSIZE(items); n++)
-				{
-					const bool is_selected = (item_current_idx == n);
-					if (ImGui::Selectable(items[n], is_selected))
-						item_current_idx = n;
-
-					// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-					if (is_selected)
-						ImGui::SetItemDefaultFocus();
-				}
-				ImGui::EndListBox();
-			}
-
-			// Custom size: use all width, 5 items tall
-			ImGui::Text("Full-width:");
-			if (ImGui::BeginListBox("##listbox 2", ImVec2(-FLT_MIN, 5 * ImGui::GetTextLineHeightWithSpacing())))
-			{
-				for (int n = 0; n < IM_ARRAYSIZE(items); n++)
-				{
-					const bool is_selected = (item_current_idx == n);
-					if (ImGui::Selectable(items[n], is_selected))
-						item_current_idx = n;
-
-					// Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-					if (is_selected)
-						ImGui::SetItemDefaultFocus();
-				}
-				ImGui::EndListBox();
-			}
-
-			ImGui::TreePop();
-		}
-
-		ImGui::End();
-
-
-		if (clear_color_changed) {
-			change_clear_color(clear_color[0], clear_color[1], clear_color[2]);
-		}
-
-	}
-
-private:
-	float clear_color[3] = { .0f, .0f, .0f };
-	void change_clear_color(float r, float g, float b) {
-		glClearColor(r, g, b, 1.00f);
-	}
-};
 
 struct Vec2 {
     float x, y;
@@ -155,13 +67,13 @@ public:
                 cohesion = cohesion + other.position;
 
                 // Избегание
-                separation = separation + (position - other.position) * (1.0f / distance);
+				separation = separation + (position - other.position) * (1.0f / distance);
                 total++;
             }
         }
 
         if (total > 0) {
-            alignment = alignment * (1.0f / total);
+            alignment = (alignment * (1.0f / total)).normalized() * max_speed;;
             cohesion = (cohesion * (1.0f / total) - position) * 0.01f;
             separation = separation * 0.1f;
 
@@ -175,6 +87,12 @@ public:
 
         // Обновляем позицию
         position = position + velocity;
+
+        // Обработка выхода за границы экрана
+        if (position.x < 0) position.x += W;
+        if (position.x > W) position.x -= W;
+        if (position.y < 0) position.y += H;
+        if (position.y > H) position.y -= H;		
     }
 };
 
@@ -188,8 +106,6 @@ int main()
 	const char *glsl_version = "#version 130";
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
 	// Create window with graphics context
 	GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui - Example", NULL, NULL);
@@ -214,19 +130,6 @@ int main()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	CustomImGui myimgui;
-	myimgui.Init(window, glsl_version);
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-		glClear(GL_COLOR_BUFFER_BIT);
-		myimgui.NewFrame();
-		myimgui.Update();
-		myimgui.Render();
-		glfwSwapBuffers(window);
-
-	}
-	myimgui.Shutdown();
-
 	const int NUM_BOIDS = 100;
 	std::vector<Boid> boids;
 
@@ -246,14 +149,12 @@ int main()
 
 		// Отрисовка боидов
 		for (const Boid& boid : boids) {
-			glBegin(GL_POINTS);
-			glVertex2f(boid.position.x, boid.position.y);
+			glBegin(GL_TRIANGLES);
+			glVertex2f(boid.position.x, boid.position.y - 5); // Нижняя точка
+			glVertex2f(boid.position.x - 5, boid.position.y + 5); // Левый верхний угол
+			glVertex2f(boid.position.x + 5, boid.position.y + 5); // Правый верхний угол
 			glEnd();
 		}
-
-		myimgui.NewFrame();
-		myimgui.Update();
-		myimgui.Render();
 		glfwSwapBuffers(window);
 	}
 	return 0;
